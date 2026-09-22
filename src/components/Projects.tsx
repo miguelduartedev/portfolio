9cab0a86f77050085ee7116ef403952f49cbbb25
miguelduartedev <miguelduarte.contact@gmail.com>
2026-09-22 22:52:03 +0300

    Update projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -3,14 +3,6 @@ import { ArrowLeft, ArrowRight, ArrowUpRight } from "lucide-react"
 
 const projects = [
   {
-    title: "Interactive Maps",
-    tags: ["React", "Next.JS", "Redux TK"],
-    desc: "Interactive Maps is a custom map making tool that I've worked on out of passion for Maps, Geography and Geopolitics.",
-    image: "/portfolio/interactivemaps.png",
-    href: "https://interactive-maps.vercel.app/",
-    color: "hover:border-primary/20 hover:text-primary",
-  },
-  {
     title: "SwimCity",
     tags: ["React Native", "TypeScript", "Jest"],
     desc: "A mobile app for finding Helsinki swimming spots with live water data, interactive maps, favourites and beach details.",
@@ -40,6 +32,14 @@ const projects = [
     image: "/portfolio/LocalDrop.png",
     href: "",
     color: "hover:border-secondary/20 hover:text-secondary",
+  },
+  {
+    title: "Interactive Maps",
+    tags: ["React", "Next.JS", "Redux TK"],
+    desc: "Interactive Maps is a custom map making tool that I've worked on out of passion for Maps, Geography and Geopolitics.",
+    image: "/portfolio/interactivemaps.png",
+    href: "https://interactive-maps.vercel.app/",
+    color: "hover:border-primary/20 hover:text-primary",
   },
   {
     title: "API Request Batcher",
